8b662778bc5e28d80250b6bba7770debe480ce1b
STEPAN <chichetka27@gmail.com>
2022-06-18 18:14:24 +0400

    add 1 Filling Array

---
+++ 1_FillingArray/Program.cs
@@ -0,0 +1,18 @@
+﻿// Заполнение массива с клавиатуры.
+
+Console.Clear();
+
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n]; // n - элементов (можно цифру указать);
+
+for (int i = 0; i < array.Length; i++) // ввод массива с клавиатуры;
+{
+    Console.Write($"Введите элемент с индексом {i} -> ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < array.Length; i++) // вывод массива;
+{                                      // array.Length = n;
+    Console.Write(array[i] + " ");
+}
